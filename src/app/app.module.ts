05cc1c825d9923ad2d63a52c8dda5d0a66814030
hareshbhai <haresh.prajapati@outlook.com>
2020-04-03 13:22:40 +0530

    Saved an application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WarningComponent } from 'src/warning/warning.component';
+import { SuccessComponent } from 'src/success/success.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningComponent,
+    SuccessComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule
